3cfd3a1de4d4b8210bb20997212afa26fa91fa3b
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-02-03 10:20:57 +0100

    adding number buttons

--- src/Calculator.java
+++ src/Calculator.java
@@ -55,6 +55,13 @@ public class Calculator implements ActionListener{
 			functionButtons[i].setFocusable(false);
 		}
 		
+		for(int i = 0; i < 10; i++){
+			numberButtons[i] = new JButton(String.valueOf(i));
+			numberButtons[i].addActionListener(this);
+			numberButtons[i].setFont(myFont);
+			numberButtons[i].setFocusable(false);
+		}
+		
 		frame.add(textField);
 		frame.setVisible(true);
 	}
