f83b1bd400e93e09ddf556b847592594b8658927
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-02-03 10:26:43 +0100

    Displaying buttons, and font style changed

--- src/Calculator.java
+++ src/Calculator.java
@@ -12,7 +12,7 @@ public class Calculator implements ActionListener{
 	JButton decButton, equButton, delButton, clrButton;
 	JPanel panel;
 	
-	Font myFont = new Font("Calibri", Font.BOLD,30);
+	Font myFont = new Font("Calibri", Font.PLAIN,25);
 	
 	double num1 = 0, num2 = 0, result = 0;
 	char operator;
@@ -62,6 +62,33 @@ public class Calculator implements ActionListener{
 			numberButtons[i].setFocusable(false);
 		}
 		
+		delButton.setBounds(50,430,145,50);
+		clrButton.setBounds(205,430,145,50);
+		
+		panel = new JPanel();
+		panel.setBounds(50,100,300,300);
+		panel.setLayout(new GridLayout(4,4,10,10));
+		
+		panel.add(numberButtons[1]);
+		panel.add(numberButtons[2]);
+		panel.add(numberButtons[3]);
+		panel.add(addButton);
+		panel.add(numberButtons[4]);
+		panel.add(numberButtons[5]);
+		panel.add(numberButtons[6]);
+		panel.add(subButton);
+		panel.add(numberButtons[7]);
+		panel.add(numberButtons[8]);
+		panel.add(numberButtons[9]);
+		panel.add(mulButton);
+		panel.add(decButton);
+		panel.add(numberButtons[0]);
+		panel.add(equButton);
+		panel.add(divButton);
+		
+		frame.add(panel);
+		frame.add(delButton);
+		frame.add(clrButton);
 		frame.add(textField);
 		frame.setVisible(true);
 	}
